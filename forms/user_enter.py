068ae401b15404be61a70dcec21c028167b27cde
Smart developer <developer@yandex.ru>
2022-04-05 22:44:40 +0600

    доп формы + исправленная бд

--- forms/user_enter.py
+++ forms/user_enter.py
@@ -7,7 +7,4 @@ from wtforms.validators import DataRequired
 class RegisterForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    about = TextAreaField("Немного о себе")
     submit = SubmitField('Войти')
