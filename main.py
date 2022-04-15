119c21d14daeaf1792dece2c0119aa839136157b
Smart developer <developer@yandex.ru>
2022-04-16 00:31:32 +0600

    реквайрементс тхт (спать хочу снова)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from data import db_session
+import os
 from data.users import User
 from forms.user_register import RegisterForm
 from flask_login import LoginManager, login_user, login_required, logout_user
@@ -147,7 +148,8 @@ def view_user(id):
 
 def main():
     db_session.global_init("db/content.db")
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
