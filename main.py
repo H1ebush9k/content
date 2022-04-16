c883a2ec3e4d4136ee863cbd8aea7bf6bbfcf334
Smart developer <developer@yandex.ru>
2022-04-16 15:45:24 +0600

    просто комит

--- main.py
+++ main.py
@@ -148,8 +148,8 @@ def view_user(id):
 
 def main():
     db_session.global_init("db/content.db")
-    # port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     main()
